624b204e8bdd198e7fe500c71204849a898b1ade
MichaelLSmith <mlsmith45@gmail.com>
2017-05-26 18:05:38 -0400

    added redux & localStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers'
+import { loadState, saveState } from './helpers/localStorage'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const persistedState = loadState()
+
+const store = createStore(
+  reducers,
+  persistedState
+//applyMiddleware
+)
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
+console.log(store.getState())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
+registerServiceWorker()
